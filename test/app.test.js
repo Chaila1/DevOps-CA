3ba3345de7b66bd1cf8c9ee3931ae288380bd560
Chaila1 <115548186+Chaila1@users.noreply.github.com>
2026-03-15 17:28:49 +0000

    Tests Commit

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const req = require('supertest');
+const app = require('./app');
+
+describe('Get /', () => {
+    it('should return 200 OK', (done) => {
+        request(app)
+        .get('/')
+        .expect(200, done);
+    });
+});
